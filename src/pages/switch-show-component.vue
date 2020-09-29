e951c878f3822ab06f540eff645e515bf4c4eae2
AerospaceXu <aerospacexu@gmail.com>
2020-09-29 10:48:14 +0800

    feat: 新增 switch 组件的指引

--- src/pages/switch-show-component.vue
+++ src/pages/switch-show-component.vue
@@ -1,5 +1,9 @@
 <template>
   <switch-component v-model:active="active" />
+  <h2>组件使用</h2>
+  <code>
+    &lt;switch-component v-model:active=&quot;your-variable&quot; /&gt;
+  </code>
 </template>
 
 <script lang="ts">
