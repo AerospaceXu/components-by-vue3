29c6637440facd0d567228f5231b4d8daef238f3
AerospaceXu <aerospacexu@gmail.com>
2020-10-01 18:00:01 +0800

    fix: 更新路径

---
+++ dist/assets/about-page-component.fcae585d.js
@@ -0,0 +1,1 @@
+let e=document.createElement("style");e.innerHTML="",document.head.appendChild(e);import{o as n,c as t,F as a,a as r}from"./index.acf6d0cb.js";var u={setup:()=>({})};const c=r("h1",null,"版本：Vue3.0.0-rc.1",-1),l=r("p",null,"这是基于 Vue3-rc.1 版本的实验性组件库",-1);u.render=function(e,r,u,o,s,d){return n(),t(a,null,[c,l],64)},u.__file="src/views/about-page-component.vue";export default u;
